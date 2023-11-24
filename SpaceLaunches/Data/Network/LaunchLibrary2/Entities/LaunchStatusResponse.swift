8b5c2df9706e1c351aed5edc40682cbb5c2f564a
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-11-24 16:32:00 +0100

    create `LaunchStatusResponse` structure

---
+++ SpaceLaunches/Data/Network/LaunchLibrary2/Entities/LaunchStatusResponse.swift
@@ -0,0 +1,19 @@
+/// The response data for the launch status information.
+struct LaunchStatusResponse: Codable {
+    /// The unique identifier for the launch status.
+    let id: Int
+    /// The name of the launch status.
+    let name: String
+    /// The abbreviated representation of the launch status.
+    let abbrev: String
+    /// The description of the launch status.
+    let description: String
+
+    /// Coding keys to map the structure to JSON keys during encoding and decoding.
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case abbrev
+        case description
+    }
+}
