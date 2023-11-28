bd36d89c45cee5dda388dcd74a00c40cf3d36136
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-11-28 11:56:55 +0100

    fix doc comments for `LaunchListResponse` to clarify

--- SpaceLaunches/Data/Network/LaunchLibrary2/Entities/LaunchListResponse.swift
+++ SpaceLaunches/Data/Network/LaunchLibrary2/Entities/LaunchListResponse.swift
@@ -1,10 +1,10 @@
-/// Paginated response for the list of launches.
+/// A paginated response containing a list of launches, designed for decoding JSON responses.
 struct LaunchListResponse: Decodable {
-    /// The total count of launches available.
+    /// The total count of launches available in the entire dataset.
     let count: Int
-    /// The URL for the next page of launches, if available.
+    /// The URL for the next page of launches, if available. `nil` if there is no next page.
     let next: String?
-    /// The URL for the previous page of launches, if available.
+    /// The URL for the previous page of launches, if available. `nil` if there is no previous page.
     let previous: String?
     /// Contains `LaunchResponse` objects representing launches in the current response.
     let results: [LaunchResponse]
