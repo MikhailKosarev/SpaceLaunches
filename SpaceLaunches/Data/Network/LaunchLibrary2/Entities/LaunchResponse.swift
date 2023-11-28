d0e11b95fdc5fee6fe11f9648d720b10c1f5c4de
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-11-28 11:58:17 +0100

    fix doc comments for `LaunchResponse` to clarify

--- SpaceLaunches/Data/Network/LaunchLibrary2/Entities/LaunchResponse.swift
+++ SpaceLaunches/Data/Network/LaunchLibrary2/Entities/LaunchResponse.swift
@@ -1,22 +1,22 @@
-/// The response data for the launch.
+/// The response data for a launch, designed for decoding JSON responses.
 struct LaunchResponse: Decodable {
     /// The unique identifier for the launch.
     let id: String
     /// The name of the launch.
     let name: String
-    /// The status of the launch, if available.
+    /// The status of the launch, `nil` if unavailable.
     let status: LaunchStatusResponse?
-    /// The scheduled launch date and time in ISO 8601 format.
+    /// The scheduled launch date and time in ISO 8601 format, `nil` if unavailable.
     let net: String?
-    /// Information about the launch service provider, if available.
+    /// Information about the launch service provider, `nil` if unavailable.
     let launchServiceProvider: LaunchServiceProviderResponse?
-    /// Information about the rocket used in the launch, if available.
+    /// Information about the rocket used in the launch, `nil` if unavailable.
     let rocket: RocketResponse?
-    /// Information about the mission associated with the launch, if available.
+    /// Information about the mission associated with the launch, `nil` if unavailable.
     let mission: MissionResponse?
-    /// Information about the launch pad, if available.
+    /// Information about the launch pad, `nil` if unavailable.
     let pad: PadResponse?
-    /// The URL to an image related to the launch, if available.
+    /// The URL to an image related to the launch, `nil` if unavailable.
     let image: String?
 
     /// Coding keys to map the structure to JSON keys during encoding and decoding.
