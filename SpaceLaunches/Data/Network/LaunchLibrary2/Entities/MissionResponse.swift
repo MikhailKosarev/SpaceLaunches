e2013cd0690f9c7944591a8c916a94a9a3587fb2
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-11-28 11:58:55 +0100

    fix doc comments for `MissionResponse` to clarify

--- SpaceLaunches/Data/Network/LaunchLibrary2/Entities/MissionResponse.swift
+++ SpaceLaunches/Data/Network/LaunchLibrary2/Entities/MissionResponse.swift
@@ -1,4 +1,4 @@
-/// The response data for the mission information.
+/// The response data for mission information, designed for decoding JSON responses.
 struct MissionResponse: Decodable {
     /// The unique identifier for the mission.
     let id: Int
@@ -8,9 +8,9 @@ struct MissionResponse: Decodable {
     let description: String
     /// The type of the mission.
     let type: String
-    /// The orbit details associated with the mission, if available.
+    /// The orbit details associated with the mission, `nil` if unavailable.
     let orbit: OrbitResponse?
-    /// The agencies involved in the mission, if available.
+    /// The agencies involved in the mission, `nil` if unavailable.
     let agencies: [AgencyResponse]?
 
     /// Coding keys to map the structure to JSON keys during encoding and decoding.
