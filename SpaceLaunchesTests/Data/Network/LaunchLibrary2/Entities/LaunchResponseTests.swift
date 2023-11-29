ea0db45e39b7ac130b4674af343c4c921f0ad10b
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-11-29 10:30:53 +0100

    create `test_LaunchResponse_failsDecodingWhenAllRequiredValuesNull` test case

--- SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/LaunchResponseTests.swift
+++ SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/LaunchResponseTests.swift
@@ -45,5 +45,28 @@ final class LaunchResponseTests: XCTestCase {
         // When & Then
         XCTAssertNoThrow(try JSONDecoder().decode(LaunchResponse.self, from: Data(jsonWithAllOptionalValuesNull)))
     }
+
+    func test_LaunchResponse_failsDecodingWhenAllRequiredValuesNull() {
+        // Given
+        // swiftlint:disable line_length
+        let jsonWithAllRequiredValuesNull = """
+                {
+                    "id": "null",
+                    "name": "null",
+                    "status": {"id": 1, "name": "Status", "abbrev": "S", "description": "Launch Status"},
+                    "net": "2023-11-29T12:00:00Z",
+                    "launch_service_provider": {"id": 1, "name": "Provider", "logoURL": "https://example.com/logo"},
+                    "rocket": {"id": 1, "configuration": {"id": 1, "name": "Rocket", "imageURL": "https://example.com/image"}},
+                    "mission": {"id": 1, "name": "Mission", "description": "Mission Description", "type": "Type", "orbit": {"id": 1, "name": "Orbit", "abbrev": "O"}},
+                    "pad": {"id": 1, "latitude": "28.5623", "longitude": "-80.5774", "location": {"id": 1, "name": "Cape Canaveral"}},
+                    "image": "https://example.com/image"
+                }
+                """.utf8
+        // swiftlint:enable line_length
+
+        // When & Then
+        XCTAssertThrowsError(try JSONDecoder().decode(LaunchListResponse.self,
+                                                      from: Data(jsonWithAllRequiredValuesNull)))
+    }
     }
 }
