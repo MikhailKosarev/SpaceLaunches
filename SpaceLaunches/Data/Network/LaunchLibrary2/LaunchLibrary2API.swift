937f9fbef1f7ffbbc42dedb3b870725732feb2ae
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-11-29 13:24:54 +0100

    confirm `LaunchLibrary2API` to the `TargetType` protocol

--- SpaceLaunches/Data/Network/LaunchLibrary2/LaunchLibrary2API.swift
+++ SpaceLaunches/Data/Network/LaunchLibrary2/LaunchLibrary2API.swift
@@ -13,3 +13,43 @@ enum LaunchLibrary2API {
     /// - Parameter id: The identifier of the launch.
     case getLaunch(id: String)
 }
+
+// MARK: - TargetType
+extension LaunchLibrary2API: TargetType {
+    /// The target's base `URL`.
+    var baseURL: URL {
+        Credentials.LaunchLibrary2.baseURL
+    }
+    /// The path to be appended to `baseURL` to form the full `URL`.
+    var path: String {
+        switch self {
+        case .getLaunchList(let type, _):
+            return "launch/\(type.rawValue)"
+        case .getLaunch(let id):
+            return "launch/\(id)"
+        }
+    }
+    /// The HTTP method used in the request.
+    var method: Method {
+        switch self {
+        case .getLaunchList, .getLaunch:
+            return .get
+        }
+    }
+    /// The type of HTTP task to be performed.
+    var task: Task {
+        switch self {
+        case .getLaunchList(_, let parameters):
+            return .requestParameters(parameters: ["limit": parameters.limit,
+                                                   "offset": parameters.offset,
+                                                   "mode": parameters.mode, ],
+                                      encoding: URLEncoding.default)
+        case .getLaunch:
+            return .requestPlain
+        }
+    }
+    /// The headers to be used in the request.
+    var headers: [String: String]? {
+        nil
+    }
+}
