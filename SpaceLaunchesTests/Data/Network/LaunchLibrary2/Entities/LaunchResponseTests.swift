5d1f23e1b294e4beb361a13fd1abc18e770b3570
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-11-29 10:42:47 +0100

    fix JSON formatting

--- SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/LaunchResponseTests.swift
+++ SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/LaunchResponseTests.swift
@@ -8,18 +8,18 @@ final class LaunchResponseTests: XCTestCase {
         // Given
         // swiftlint:disable line_length
         let validJSON = """
-                {
-                    "id": "1",
-                    "name": "Launch 1",
-                    "status": {"id": 1, "name": "Status", "abbrev": "S", "description": "Launch Status"},
-                    "net": "2023-11-29T12:00:00Z",
-                    "launch_service_provider": {"id": 1, "name": "Provider", "logoURL": "https://example.com/logo"},
-                    "rocket": {"id": 1, "configuration": {"id": 1, "name": "Rocket", "imageURL": "https://example.com/image"}},
-                    "mission": {"id": 1, "name": "Mission", "description": "Mission Description", "type": "Type", "orbit": {"id": 1, "name": "Orbit", "abbrev": "O"}},
-                    "pad": {"id": 1, "latitude": "28.5623", "longitude": "-80.5774", "location": {"id": 1, "name": "Cape Canaveral"}},
-                    "image": "https://example.com/image"
-                }
-                """.utf8
+            {
+                "id": "1",
+                "name": "Launch 1",
+                "status": {"id": 1, "name": "Status", "abbrev": "S", "description": "Launch Status"},
+                "net": "2023-11-29T12:00:00Z",
+                "launch_service_provider": {"id": 1, "name": "Provider", "logoURL": "https://example.com/logo"},
+                "rocket": {"id": 1, "configuration": {"id": 1, "name": "Rocket", "imageURL": "https://example.com/image"}},
+                "mission": {"id": 1, "name": "Mission", "description": "Mission Description", "type": "Type", "orbit": {"id": 1, "name": "Orbit", "abbrev": "O"}},
+                "pad": {"id": 1, "latitude": "28.5623", "longitude": "-80.5774", "location": {"id": 1, "name": "Cape Canaveral"}},
+                "image": "https://example.com/image"
+            }
+            """.utf8
         // swiftlint:enable line_length
 
         // When & Then
@@ -29,18 +29,18 @@ final class LaunchResponseTests: XCTestCase {
     func test_LaunchResponse_successfullyDecodedWhenAllOptionalValuesAreNull() {
         // Given
         let jsonWithAllOptionalValuesNull = """
-                {
-                    "id": "1",
-                    "name": "Launch 1",
-                    "status": null,
-                    "net": null,
-                    "launch_service_provider": null,
-                    "rocket": null,
-                    "mission": null,
-                    "pad": null,
-                    "image": null
-                }
-                """.utf8
+            {
+                "id": "1",
+                "name": "Launch 1",
+                "status": null,
+                "net": null,
+                "launch_service_provider": null,
+                "rocket": null,
+                "mission": null,
+                "pad": null,
+                "image": null
+            }
+            """.utf8
 
         // When & Then
         XCTAssertNoThrow(try JSONDecoder().decode(LaunchResponse.self, from: Data(jsonWithAllOptionalValuesNull)))
@@ -50,18 +50,18 @@ final class LaunchResponseTests: XCTestCase {
         // Given
         // swiftlint:disable line_length
         let jsonWithAllRequiredValuesNull = """
-                {
-                    "id": "null",
-                    "name": "null",
-                    "status": {"id": 1, "name": "Status", "abbrev": "S", "description": "Launch Status"},
-                    "net": "2023-11-29T12:00:00Z",
-                    "launch_service_provider": {"id": 1, "name": "Provider", "logoURL": "https://example.com/logo"},
-                    "rocket": {"id": 1, "configuration": {"id": 1, "name": "Rocket", "imageURL": "https://example.com/image"}},
-                    "mission": {"id": 1, "name": "Mission", "description": "Mission Description", "type": "Type", "orbit": {"id": 1, "name": "Orbit", "abbrev": "O"}},
-                    "pad": {"id": 1, "latitude": "28.5623", "longitude": "-80.5774", "location": {"id": 1, "name": "Cape Canaveral"}},
-                    "image": "https://example.com/image"
-                }
-                """.utf8
+            {
+                "id": "null",
+                "name": "null",
+                "status": {"id": 1, "name": "Status", "abbrev": "S", "description": "Launch Status"},
+                "net": "2023-11-29T12:00:00Z",
+                "launch_service_provider": {"id": 1, "name": "Provider", "logoURL": "https://example.com/logo"},
+                "rocket": {"id": 1, "configuration": {"id": 1, "name": "Rocket", "imageURL": "https://example.com/image"}},
+                "mission": {"id": 1, "name": "Mission", "description": "Mission Description", "type": "Type", "orbit": {"id": 1, "name": "Orbit", "abbrev": "O"}},
+                "pad": {"id": 1, "latitude": "28.5623", "longitude": "-80.5774", "location": {"id": 1, "name": "Cape Canaveral"}},
+                "image": "https://example.com/image"
+            }
+            """.utf8
         // swiftlint:enable line_length
 
         // When & Then
@@ -72,17 +72,17 @@ final class LaunchResponseTests: XCTestCase {
     func test_LaunchResponse_failsDecodingWhenRequiredKeyIsMissing() {
         // Given
         let jsonWithoutIdKey = """
-                {
-                    "name": "Launch 1",
-                    "status": null,
-                    "net": null,
-                    "launch_service_provider": null,
-                    "rocket": null,
-                    "mission": null,
-                    "pad": null,
-                    "image": null
-                }
-                """.utf8
+            {
+                "name": "Launch 1",
+                "status": null,
+                "net": null,
+                "launch_service_provider": null,
+                "rocket": null,
+                "mission": null,
+                "pad": null,
+                "image": null
+            }
+            """.utf8
 
         // When & Then
         XCTAssertThrowsError(try JSONDecoder().decode(LaunchResponse.self, from: Data(jsonWithoutIdKey)))
@@ -91,18 +91,18 @@ final class LaunchResponseTests: XCTestCase {
     func test_LaunchResponse_failsDecodingWhenKeyIsNotValidType() {
         // Given
         let jsonWithInvalidId = """
-                {
-                    "id": 1,
-                    "name": "Launch 1",
-                    "status": null,
-                    "net": null,
-                    "launch_service_provider": null,
-                    "rocket": null,
-                    "mission": null,
-                    "pad": null,
-                    "image": null
-                }
-                """.utf8
+            {
+                "id": 1,
+                "name": "Launch 1",
+                "status": null,
+                "net": null,
+                "launch_service_provider": null,
+                "rocket": null,
+                "mission": null,
+                "pad": null,
+                "image": null
+            }
+            """.utf8
 
         // When & Then
         XCTAssertThrowsError(try JSONDecoder().decode(LaunchResponse.self, from: Data(jsonWithInvalidId)))
