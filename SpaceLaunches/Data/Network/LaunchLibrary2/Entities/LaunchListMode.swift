700c0f7a13f16ab197ed58f35efb68276774397b
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-11-29 12:48:47 +0100

    create `LaunchListMode` enum

---
+++ SpaceLaunches/Data/Network/LaunchLibrary2/Entities/LaunchListMode.swift
@@ -0,0 +1,9 @@
+/// Mode for obtaining launch list information.
+enum LaunchListMode {
+    /// General launch list information.
+    case normal
+    /// Concise summary of launch list details.
+    case list
+    /// Comprehensive launch list information.
+    case detailed
+}
