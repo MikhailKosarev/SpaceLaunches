1482006021d45871defd346e7e924502e3d91a9d
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-11-30 11:13:09 +0100

    create `test_LaunchStatusResponse_successfullyDecodedWhenValidJSONIsProvided` test case

--- SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/LaunchStatusResponseTests.swift
+++ SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/LaunchStatusResponseTests.swift
@@ -3,4 +3,19 @@ import XCTest
 @testable import SpaceLaunches
 
 final class LaunchStatusResponseTests: XCTestCase {
+
+    func test_LaunchStatusResponse_successfullyDecodedWhenValidJSONIsProvided() {
+        // Given
+        let validJSON = """
+            {
+                "id": 1,
+                "name": "Status",
+                "abbrev": "S",
+                "description": "Launch Status"
+            }
+            """.utf8
+
+        // When & Then
+        XCTAssertNoThrow(try JSONDecoder().decode(LaunchStatusResponse.self, from: Data(validJSON)))
+    }
 }
