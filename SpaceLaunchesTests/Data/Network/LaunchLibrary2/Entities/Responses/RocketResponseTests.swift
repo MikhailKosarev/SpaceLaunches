fb7a95abd4367efcc9f47543e5879b9a6018a341
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-04 11:07:06 +0100

    create `test_RocketResponse_successfullyDecodedWhenValidJSONIsProvided` test case

--- SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/RocketResponseTests.swift
+++ SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/RocketResponseTests.swift
@@ -4,4 +4,20 @@ import XCTest
 
 final class RocketResponseTests: XCTestCase {
 
+    func test_RocketResponse_successfullyDecodedWhenValidJSONIsProvided() {
+        // Given
+        let validJSON = """
+            {
+                "id": 1,
+                "configuration": {
+                    "id": 1,
+                    "name": "Falcon 9",
+                    "image_url": "https://example.com/image"
+                }
+            }
+            """.utf8
+
+        // When & Then
+        XCTAssertNoThrow(try JSONDecoder().decode(RocketResponse.self, from: Data(validJSON)))
+    }
 }
