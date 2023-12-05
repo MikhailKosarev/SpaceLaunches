f78f06144a59300ce12d0357b9e9db83addee1c2
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-05 21:07:11 +0100

    create `test_RocketConfigurationResponse_failsDecodingWhenAllRequiredValuesNull` test case

--- SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/RocketConfigurationResponseTests.swift
+++ SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/RocketConfigurationResponseTests.swift
@@ -19,4 +19,19 @@ final class RocketConfigurationResponseTests: XCTestCase {
                                                   from: Data(validJSON)))
     }
 
+    func test_RocketConfigurationResponse_failsDecodingWhenAllRequiredValuesNull() {
+        // Given
+        let allNullJSON = """
+        {
+            "id": null,
+            "name": null,
+            "image_url": null
+        }
+        """.utf8
+
+        // When & Then
+        XCTAssertThrowsError(try JSONDecoder().decode(RocketConfigurationResponse.self,
+                                                      from: Data(allNullJSON)))
+    }
+
 }
