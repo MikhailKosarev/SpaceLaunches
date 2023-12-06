88b3dd74f3f6e0548989205278d58b98aaf05322
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-06 11:26:18 +0100

    create `MockResponseData`

---
+++ SpaceLaunchesTests/Data/Network/Common/Mocks/MockResponseData.swift
@@ -0,0 +1,9 @@
+/// A Codable and Equatable struct representing mock response data.
+struct MockResponseData: Codable, Equatable {
+    /// The unique identifier for the response data.
+    var id = 1
+    /// The name associated with the response data.
+    var name = "Falcon"
+    /// The status of the response data.
+    var status = "Success"
+}
