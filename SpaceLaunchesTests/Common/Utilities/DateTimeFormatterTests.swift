109584f8ae91e10692cf3b7b0bce5ac7431edac1
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-08 14:07:33 +0100

    set up enviroment

--- SpaceLaunchesTests/Common/Utilities/DateTimeFormatterTests.swift
+++ SpaceLaunchesTests/Common/Utilities/DateTimeFormatterTests.swift
@@ -3,4 +3,18 @@ import XCTest
 @testable import SpaceLaunches
 
 final class DateTimeFormatterTests: XCTestCase {
+
+    private var formatter: DateTimeFormatter!
+
+    // MARK: - Setup and Teardown
+    override func setUp() {
+        super.setUp()
+        formatter = DateTimeFormatter()
+    }
+
+    override func tearDown() {
+        formatter = nil
+        super.tearDown()
+    }
+
 }
