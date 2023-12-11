63c4183c5f4eb8d56da6bec47351cf3002f157aa
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-11 10:15:57 +0100

    create `test_OrbitResponse_successfullyDecodedWhenValidJSONIsProvided` test case

--- SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/OrbitResponseTests.swift
+++ SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/OrbitResponseTests.swift
@@ -3,4 +3,18 @@ import XCTest
 @testable import SpaceLaunches
 
 final class OrbitResponseTests: XCTestCase {
+
+    func test_OrbitResponse_successfullyDecodedWhenValidJSONIsProvided() {
+        // Given
+        let validJSON = """
+            {
+                "id": 1,
+                "name": "Low Earth Orbit",
+                "abbrev": "LEO"
+            }
+            """.utf8
+
+        // When & Then
+        XCTAssertNoThrow(try JSONDecoder().decode(OrbitResponse.self, from: Data(validJSON)))
+    }
 }
