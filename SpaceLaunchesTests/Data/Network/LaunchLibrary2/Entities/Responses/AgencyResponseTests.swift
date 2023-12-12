d52998585a2993962492194cbbb3cb9e7e399e5a
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-12 15:34:38 +0100

    create `test_AgencyResponse_successfullyDecodedWhenValidJSONIsProvided` test case

--- SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/AgencyResponseTests.swift
+++ SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/AgencyResponseTests.swift
@@ -3,4 +3,18 @@ import XCTest
 @testable import SpaceLaunches
 
 final class AgencyResponseTests: XCTestCase {
+
+    func test_AgencyResponse_successfullyDecodedWhenValidJSONIsProvided() {
+        // Given
+        let validJSON = """
+            {
+                "id": 1,
+                "name": "NASA",
+                "logo_url": "https://example.com/nasa_logo.png"
+            }
+            """.utf8
+
+        // When & Then
+        XCTAssertNoThrow(try JSONDecoder().decode(AgencyResponse.self, from: Data(validJSON)))
+    }
 }
