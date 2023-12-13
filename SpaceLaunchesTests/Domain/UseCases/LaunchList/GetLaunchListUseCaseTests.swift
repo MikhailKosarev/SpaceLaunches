7dfe8982de24d4a6723a6eabbbacd15e97b5fcb3
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-13 13:15:59 +0100

    set up `setUp` and `tearDown`

--- SpaceLaunchesTests/Domain/UseCases/LaunchList/GetLaunchListUseCaseTests.swift
+++ SpaceLaunchesTests/Domain/UseCases/LaunchList/GetLaunchListUseCaseTests.swift
@@ -6,4 +6,22 @@ import XCTest
 @testable import SpaceLaunches
 
 final class GetLaunchListUseCaseTests: XCTestCase {
+
+    // MARK: - Setup and Teardown
+    override func setUp() {
+        super.setUp()
+        bag = DisposeBag()
+        scheduler = TestScheduler(initialClock: 0)
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        bag = nil
+        scheduler = nil
+    }
+
+
+    // MARK: - Helper properties and methods
+    private var scheduler: TestScheduler!
+    private var bag: DisposeBag!
 }
