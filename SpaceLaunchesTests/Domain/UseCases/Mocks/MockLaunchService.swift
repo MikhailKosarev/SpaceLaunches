8d28fd28e92efe6fde50f076c0fa98998086fec7
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-13 13:07:01 +0100

    create `MockLaunchService` class

---
+++ SpaceLaunchesTests/Domain/UseCases/Mocks/MockLaunchService.swift
@@ -0,0 +1,7 @@
+import RxSwift
+
+@testable import SpaceLaunches
+
+/// A mock implementation of the `LaunchService` protocol for testing purposes.
+final class MockLaunchService: LaunchService {
+}
