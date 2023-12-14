5081ce48bd520e79bcd334afe0dbef409bf490dc
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-14 10:22:04 +0100

    write a default implementation of the `LaunchListViewController`

--- SpaceLaunches/Presentation/LaunchList/LaunchListViewController.swift
+++ SpaceLaunches/Presentation/LaunchList/LaunchListViewController.swift
@@ -1,1 +1,11 @@
+import UIKit
 
+/// View controller for displaying a list of launches.
+final class LaunchListViewController: UIViewController {
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
