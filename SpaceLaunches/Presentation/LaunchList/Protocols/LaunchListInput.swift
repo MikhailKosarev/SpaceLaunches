2265e4fbe54736545d8eb8662e53a1003c2d0a8a
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-15 10:20:39 +0100

    define `LaunchListInput` protocol

---
+++ SpaceLaunches/Presentation/LaunchList/Protocols/LaunchListInput.swift
@@ -0,0 +1,15 @@
+import RxCocoa
+import RxSwift
+
+/// Protocol defining the input for a launch list.
+protocol LaunchListInput {
+
+    /// Driver emitting a void event when the view is loaded.
+    var viewdDidLoad: Driver<Void> { get }
+    /// Driver emitting the selected launch item.
+    var selectedLaunch: Driver<LaunchListItem> { get }
+    /// Driver emitting an array of row indices to prefetch.
+    var rowsToPrefetch: Driver<[Int]> { get }
+    /// Driver emitting the selected type of launches to display.
+    var selectedLaunchesType: Driver<LaunchListDisplayType> { get }
+}
