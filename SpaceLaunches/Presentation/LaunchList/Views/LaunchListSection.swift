e523c675874a04c2e48748e414a35dd5f9c70539
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-15 10:37:05 +0100

    create `LaunchListSection`

---
+++ SpaceLaunches/Presentation/LaunchList/Views/LaunchListSection.swift
@@ -0,0 +1,11 @@
+import RxDataSources
+
+/// A section for the table view containing the launch list.
+struct LaunchListSection {
+
+    /// Header title for the section.
+    var header: String
+    /// Items within the section.
+    var items: [Item]
+}
+
