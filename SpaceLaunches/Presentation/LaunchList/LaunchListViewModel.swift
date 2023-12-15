78579532e2ffcde75698cf35a68f21776610e0a2
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-15 11:17:41 +0100

    create a private interface with initialization

--- SpaceLaunches/Presentation/LaunchList/LaunchListViewModel.swift
+++ SpaceLaunches/Presentation/LaunchList/LaunchListViewModel.swift
@@ -3,4 +3,28 @@ import RxSwift
 
 /// View model for managing the launch list.
 final class LaunchListViewModel {
+
+    // MARK: - Private interface
+    /// Dispose bag to store subscriptions.
+    private let bag = DisposeBag()
+    /// Service for fetching launch data.
+    private let service: LaunchLibrary2Service
+    /// Use case for getting the launch list.
+    private let getLaunchListUseCase: GetLaunchListUseCase
+    /// Relay for holding the sections of the launch list.
+    private let cellsRelay = BehaviorRelay<[LaunchListSection]>(value: [])
+    /// Relay for holding the list of launches.
+    private let launchListRelay = BehaviorRelay<[LaunchListItem]>(value: [])
+    /// Number of items to load initially.
+    private let numberOfItemsToLoad = 20
+    /// Number of items to prefetch.
+    private let numberOfItemsToPrefetch = 10
+
+    // MARK: - Initialization
+    /// Initializes the launch list view model.
+    /// - Parameter service: Service for fetching launch data.
+    init(service: LaunchLibrary2Service) {
+        self.service = service
+        getLaunchListUseCase = GetLaunchListUseCase(service: service)
+    }
 }
