f4d4a1f7b7d26d4998e67e5b91da9926b7ec523e
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-18 19:38:40 +0100

    create `test_PadResponse_successfullyDecodedWhenValidJSONIsProvided` test case

--- SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/PadResponseTests.swift
+++ SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/PadResponseTests.swift
@@ -3,4 +3,22 @@ import XCTest
 @testable import SpaceLaunches
 
 final class PadResponseTests: XCTestCase {
+
+    func test_PadResponse_successfullyDecodedWhenValidJSONIsProvided() {
+        // Given
+        let validJSON = """
+            {
+                "id": 1,
+                "latitude": "28.5623",
+                "longitude": "-80.5774",
+                "location": {
+                    "id": 101,
+                    "name": "Kennedy Space Center"
+                }
+            }
+            """.utf8
+
+        // When & Then
+        XCTAssertNoThrow(try JSONDecoder().decode(PadResponse.self, from: Data(validJSON)))
+    }
 }
