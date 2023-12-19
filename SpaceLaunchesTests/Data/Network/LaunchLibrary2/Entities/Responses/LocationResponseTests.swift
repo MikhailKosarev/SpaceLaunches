209a8ccc3bd2d38356b654ded8c111ed6c091f55
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-19 19:09:06 +0100

    create `test_LocationResponse_successfullyDecodedWhenValidJSONIsProvided` test case

--- SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/LocationResponseTests.swift
+++ SpaceLaunchesTests/Data/Network/LaunchLibrary2/Entities/Responses/LocationResponseTests.swift
@@ -3,4 +3,17 @@ import XCTest
 @testable import SpaceLaunches
 
 final class LocationResponseTests: XCTestCase {
+
+    func test_LocationResponse_successfullyDecodedWhenValidJSONIsProvided() {
+        // Given
+        let validJSON = """
+            {
+                "id": 1,
+                "name": "Kennedy Space Center"
+            }
+            """.utf8
+
+        // When & Then
+        XCTAssertNoThrow(try JSONDecoder().decode(LocationResponse.self, from: Data(validJSON)))
+    }
 }
