1975a866d46bbcc6c8ab010c1ce16e7406d07296
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-12-20 20:25:53 +0100

    write implementation of the `RxReusableTableViewCell`

--- SpaceLaunches/Presentation/Common/Cells/RxReusableTableViewCell.swift
+++ SpaceLaunches/Presentation/Common/Cells/RxReusableTableViewCell.swift
@@ -4,4 +4,13 @@ import UIKit
 
 /// Custom table view cell with RxSwift integration and Reusable conformance.
 class RxReusableTableViewCell: UITableViewCell {
+
+    /// Dispose bag for managing RxSwift subscriptions.
+    private (set) var bag = DisposeBag()
+
+    /// Prepares the cell for reuse by resetting the dispose bag.
+    override func prepareForReuse() {
+        bag = DisposeBag()
+        super.prepareForReuse()
+    }
 }
