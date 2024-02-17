21a73718148cb4d42b8140e5bbc7a7fa8a950250
Mikhail Kosarev <mikhailk.developer@gmail.com>
2024-02-17 14:18:05 +0100

    imlement initialization with dependencies in the SceneDelegate

--- SpaceLaunches/Application/SceneDelegate.swift
+++ SpaceLaunches/Application/SceneDelegate.swift
@@ -10,6 +10,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
-        window?.rootViewController = LaunchListViewController()
+        window?.rootViewController = getLaunchListViewController()
+    }
+
+    private func getLaunchListViewController() -> LaunchListViewController {
+        let launchService = LaunchLibrary2Service()
+        let viewModel = LaunchListViewModel(service: launchService)
+        let viewController = LaunchListViewController(viewModel: viewModel)
+        return viewController
     }
 }
