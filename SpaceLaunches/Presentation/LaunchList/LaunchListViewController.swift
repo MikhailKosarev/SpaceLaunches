c5653f0ea828a96835ccd8960927a516a16827f5
Mikhail Kosarev <mikhailk.developer@gmail.com>
2024-02-17 14:17:23 +0100

    design initialization

--- SpaceLaunches/Presentation/LaunchList/LaunchListViewController.swift
+++ SpaceLaunches/Presentation/LaunchList/LaunchListViewController.swift
@@ -7,6 +7,20 @@ import UIKit
 /// View controller for displaying a list of launches.
 final class LaunchListViewController: UIViewController {
 
+    // MARK: - Initialization
+
+    /// Initializes the launch list view controller with a view model.
+    /// - Parameter viewModel: The view model for the launch list.
+    init(viewModel: LaunchListViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        bind()
+    }
+
+    /// Required initializer, not implemented.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
