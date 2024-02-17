05dbb12241e6979b3104ac0eaeb464c6fc70dc15
Mikhail Kosarev <mikhailk.developer@gmail.com>
2024-02-17 14:16:48 +0100

    implement UI elements

--- SpaceLaunches/Presentation/LaunchList/LaunchListViewController.swift
+++ SpaceLaunches/Presentation/LaunchList/LaunchListViewController.swift
@@ -17,5 +17,25 @@ final class LaunchListViewController: UIViewController {
     private var viewModel: LaunchListViewModelType
     private let launchListDataSource = LaunchListDataSource.dataSource
 
+    // MARK: UI elements
+    private lazy var launchListTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.register(cellType: LaunchListTableViewCell.self)
+        tableView.rx.itemSelected.bind { [weak self] in
+            tableView.deselectRow(at: $0, animated: true)
+        }.disposed(by: bag)
+        return tableView
+    }()
+
+    private lazy var launchesTypeSegmentedControl: UISegmentedControl = {
+        let items = LaunchListDisplayType.allCases.map { $0.title }
+        let segmentedControl = UISegmentedControl(items: items)
+        segmentedControl.selectedSegmentIndex = LaunchListDisplayType.upcoming.rawValue
+        return segmentedControl
+    }()
+
+    private let activityIndicatorView = UIActivityIndicatorView(style: .large)
+}
     }
 }
