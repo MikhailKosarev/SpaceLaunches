2d67e3cafe17568d47c9225336cce948931033fd
Mikhail Kosarev <mikhailk.developer@gmail.com>
2024-02-17 14:17:36 +0100

    setup viewDidLoad

--- SpaceLaunches/Presentation/LaunchList/LaunchListViewController.swift
+++ SpaceLaunches/Presentation/LaunchList/LaunchListViewController.swift
@@ -21,10 +21,14 @@ final class LaunchListViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        addSubviews()
+        setup()
+        setConstraints()
+    }
 
     // MARK: - Private interface
     private let bag = DisposeBag()
