0ea73ce9da64638fa953d28676b8765a4d0c10ae
Mikhail Kosarev <mikhailk.developer@gmail.com>
2024-03-06 20:15:32 +0100

    write doc comments for `LaunchInput`

--- SpaceLaunches/Presentation/Launch/Protocols/LaunchInput.swift
+++ SpaceLaunches/Presentation/Launch/Protocols/LaunchInput.swift
@@ -1,1 +1,5 @@
+/// Protocol defining the input expected by a `LaunchViewModel`.
+///
+/// `LaunchInput` serves as an abstraction for all possible input data or actions that can
+/// be provided to a ViewModel associated with a launch screen or feature.
 protocol LaunchInput { }
