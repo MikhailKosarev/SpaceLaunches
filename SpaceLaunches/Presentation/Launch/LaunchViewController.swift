592139f6d56bdbe256ef6685e4ed785487002e59
Mikhail Kosarev <mikhailk.developer@gmail.com>
2024-03-06 20:21:13 +0100

    add the `viewModel` and initializators to the `LaunchViewController`

--- SpaceLaunches/Presentation/Launch/LaunchViewController.swift
+++ SpaceLaunches/Presentation/Launch/LaunchViewController.swift
@@ -3,6 +3,20 @@ import UIKit
 /// View controller to display detailed information about the launch.
 final class LaunchViewController: UIViewController {
 
+    // MARK: - Private properties
+    private var viewModel: LaunchViewModelType
+
+    // MARK: - Initialization
+    init(viewModel: LaunchViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         view.backgroundColor = .yellow
     }
