9ee32bafb8211d282e1592f4e9b86f5c951a351f
Mikhail Kosarev <mikhailk.developer@gmail.com>
2024-03-06 18:30:49 +0100

    create an empty yellow `LaunchViewController`

---
+++ SpaceLaunches/Presentation/Launch/LaunchViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+/// View controller to display detailed information about the launch.
+final class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        view.backgroundColor = .yellow
+    }
+}
