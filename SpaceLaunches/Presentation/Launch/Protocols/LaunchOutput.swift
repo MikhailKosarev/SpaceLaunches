59ce39a7025c1243a3cad47855d7a57c79dc742c
Mikhail Kosarev <mikhailk.developer@gmail.com>
2024-03-06 20:16:08 +0100

    write doc comments for `LaunchOutput`

--- SpaceLaunches/Presentation/Launch/Protocols/LaunchOutput.swift
+++ SpaceLaunches/Presentation/Launch/Protocols/LaunchOutput.swift
@@ -1,1 +1,5 @@
+/// Protocol defining the output produced by a `LaunchViewModel`.
+///
+/// `LaunchOutput` serves as an abstraction for all possible outcomes or data that a ViewModel
+/// associated with a launch screen or feature can produce.
 protocol LaunchOutput { }
